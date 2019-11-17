6df547a097c99f5f547070615c954ba2dc9fc029
charlesscheuer <cscheuer@wustl.edu>
2019-11-17 16:49:53 -0600

    Updates

---
+++ precache-manifest.2e0f4af8b98aa3f72b28ea3285dba2d1.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2626e098c0700fd87786cdfd286cf33b",
+    "url": "./index.html"
+  },
+  {
+    "revision": "8bc181e790ac66ff44f2",
+    "url": "./static/css/main.f6c991d0.chunk.css"
+  },
+  {
+    "revision": "25bb909fdfefbd79346a",
+    "url": "./static/js/2.96417d06.chunk.js"
+  },
+  {
+    "revision": "8bc181e790ac66ff44f2",
+    "url": "./static/js/main.cbbb327e.chunk.js"
+  },
+  {
+    "revision": "8e8fa987e8d4a5b9210b",
+    "url": "./static/js/runtime~main.3142890b.js"
+  },
+  {
+    "revision": "7a3e627c951adbeb9794b7e26d701dcb",
+    "url": "./static/media/GT-Walsheim-Pro-Medium.7a3e627c.woff"
+  },
+  {
+    "revision": "1e587309471c4da65848615e0948f052",
+    "url": "./static/media/GT-Walsheim-Pro-Regular.1e587309.woff"
+  },
+  {
+    "revision": "f87d91c16c63b97bab0793070605b6a8",
+    "url": "./static/media/hurme-geometric-sans-3-bold.f87d91c1.woff"
+  },
+  {
+    "revision": "a972b9631c71649e3cbca46b7838852b",
+    "url": "./static/media/hurme-geometric-sans-3-regular.a972b963.woff"
+  },
+  {
+    "revision": "955382f087ae7bf2e349870bc874e6bd",
+    "url": "./static/media/hurme_geometric_oblique.955382f0.woff"
+  },
+  {
+    "revision": "5ec8f8632d56955911a3aa1f7492a1d6",
+    "url": "./static/media/hurme_geometric_sans.5ec8f863.woff"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/homepage-react-app/precache-manifest.e56be7904e898b3ec1140e553f983135.js"
+  "./precache-manifest.2e0f4af8b98aa3f72b28ea3285dba2d1.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/homepage-react-app/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
