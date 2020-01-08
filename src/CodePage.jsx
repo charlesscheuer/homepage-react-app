43b74ad807b62297c5993ffb56cbec8659546f73
charlesscheuer <cscheuer@wustl.edu>
2020-01-08 16:11:06 -0500

    fixed code page

--- src/CodePage.jsx
+++ src/CodePage.jsx
@@ -1,68 +1,98 @@
-import React, { Component } from 'react'
-import Socials from './Socials.jsx'
+import React, { Component } from 'react';
+import Socials from './Socials.jsx';
 
 export default class CodePage extends Component {
-    render() {
-        return (
-            <div>
-            <h1 class="name">Charles' Code</h1>
-       <div class="rectangle"></div>
-       <div class="content">
-            <p class="about">These are some of the web development projects I have worked on.           
-                    <br /> <br /> You can also check out my <a href="https://github.com/charlesscheuer" title="my github" class="about_email">github</a></p>
-         <div class="links">
-                <h2 class="portfolio">Technologies</h2>
-                <h4 class="portfolio_subhead">Experienced with</h4>
-                <p class="portfolio_paragraph">JavaScript, React, postgreSQL, SCSS, Ruby on Rails</p>
-                <h2 class="portfolio">Projects</h2>
-                <h4 class="portfolio_subhead">Developed in class</h4>
-                <ul class="portfolio_paragraph">
-                    <li class="portfolio_item"><a 
-                        href="https://charlesscheuer.github.io/todoappcharlesscheuer/" 
-                        title="Todo app" 
-                        class="portfolio_link" 
-                        target="blank">Todo app &#40;web only&#41;</a></li>
-                    <li class="portfolio_item"><a 
-                        href="https://charlesscheuer.github.io/background-generator/" 
-                        title="Todo app" 
-                        class="portfolio_link" 
-                        target="blank">Background Generator &#40;web only&#41;</a></li>
-                    <li class="portfolio_item"><a href="https://charlesscheuer.github.io/natours-project/" 
-                            title="Travel company project" 
-                            class="portfolio_link" 
-                            target="blank">Travel company project</a>
-                    </li>   
-                    
-                    <li class="portfolio_item"><a href="https://charlesscheuer.github.io/nexter/" 
-                        title="Realty company project" 
-                        class="portfolio_link" 
-                        target="blank">Realty company project</a>
-                    </li>  
-                </ul>
-                <h4 class="portfolio_subhead">Personal Projects</h4>
-                <ul class="portfolio_paragraph">
-                    <li class="portfolio_item"><a href="https://rickdstructurednotes.surge.sh/" 
-                        title="Spreadsheet" 
-                        class="portfolio_link" 
-                        target="blank">Live excel spreadsheets</a>
-                    </li>
-                    <li class="portfolio_item"><a href="https://www.imfree.fyi/" 
-                        title="Calendar" 
-                        class="portfolio_link" 
-                        target="blank">Calendar meeting organization</a>
-                    </li>
-                    <li class="portfolio_item"><a href="https://github.com/charlesscheuer/rocket-git" 
-                        title="Brand prototype" 
-                        class="portfolio_link" 
-                        target="blank">Fleece brand concept</a>
-                    </li>
-                    
-                </ul>
-           </div>
-            
-       </div>
-       <Socials />
-       </div>
-        )
-    }
+  render() {
+    return (
+      <div>
+        <h1 class='name'>Charles' Code</h1>
+        <div class='rectangle'></div>
+        <div class='content'>
+          <p class='about'>
+            I'm a developer that speaks the language of designers. I have two
+            years of experience as a JavaScript developer and I really enjoy
+            building side projects in React.
+            <br /> <br />
+            This summer, I was a software engineering intern at{' '}
+            <a
+              href='Flyhomes.com'
+              title='Flyhomes'
+              class='about_email'
+              target='blank'
+            >
+              Flyhomes.
+            </a>
+            <br /> <br />
+            Right now, I’m studying computer science and working on side
+            projects. If you'd like to see see the code for these projects,
+            please see my{' '}
+            <a
+              href='https://github.com/charlesscheuer'
+              title='my github'
+              class='about_email'
+            >
+              Github.
+            </a>
+          </p>
+          <div class='links'>
+            <h2 class='portfolio'>Technologies</h2>
+            <h4 class='portfolio_subhead'>I'm very comfortable working with</h4>
+            <p class='portfolio_paragraph'>
+              JavaScript, React, postgreSQL, SCSS, NodeJS
+            </p>
+            <h2 class='portfolio'>Projects</h2>
+            <h4 class='portfolio_subhead'>
+              Some personal projects I have built
+            </h4>
+            <p class='portfolio_paragraph portfolio_paragraph-code'>
+              I built{' '}
+              <a
+                href='https://www.imfree.fyi/'
+                title='Calendar'
+                class='portfolio_link'
+                target='blank'
+              >
+                ImFreeFYI
+              </a>{' '}
+              using React and NodeJS with Google Cloud Functions.
+            </p>
+            <p class='portfolio_paragraph portfolio_paragraph-code'>
+              I used React and NodeJS (and FullStory) to test the
+              click-through-rate for a{' '}
+              <a
+                href='https://github.com/charlesscheuer/rocket-git'
+                title='Brand prototype'
+                class='portfolio_link'
+                target='blank'
+              >
+                Fleece I made.
+              </a>
+            </p>
+
+            <p class='portfolio_paragraph'>
+              I'm currently hacking on a curated art collection called{' '}
+              <a
+                href='https://juried.co/home'
+                title='Juried fine art'
+                class='portfolio_link'
+                target='blank'
+              >
+                Juried
+              </a>{' '}
+              and a community for music producers called{' '}
+              <a
+                href='https://thevoltage.club'
+                title='The Voltage Club'
+                class='portfolio_link'
+                target='blank'
+              >
+                Voltage.
+              </a>
+            </p>
+          </div>
+        </div>
+        <Socials />
+      </div>
+    );
+  }
 }
--- src/index.js
+++ src/index.js
@@ -2,26 +2,34 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import * as serviceWorker from './serviceWorker';
-import {
-    HashRouter as Router,
-    Switch,
-    Route
-  } from 'react-router-dom';
-  import Home from './Home'
-  import CodePage from './CodePage'
-  import './SASS/main.scss'
-  import ElectronicMusic from './ElectronicMusic';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Home from './Home';
+import CodePage from './CodePage';
+import './SASS/main.scss';
+import ElectronicMusic from './ElectronicMusic';
 
-ReactDOM.render(<Router>
-    <Switch >
-      <Route path="/Academic" render={(props) => <Home {...props}  category={0}/>}/>
-      <Route path="/Professional" render={(props) => <Home {...props}  category={1}/>}/>
-      <Route path="/Creative" render={(props) => <Home {...props}  category={2}/>}/>
-      <Route path="/code" component={CodePage} />
-      <Route path="/electronic" component={ElectronicMusic} />
-      <Route path="/" component={Home} />
+ReactDOM.render(
+  <Router>
+    <Switch>
+      <Route
+        path='/Academic'
+        render={props => <Home {...props} category={0} />}
+      />
+      <Route
+        path='/Professional'
+        render={props => <Home {...props} category={1} />}
+      />
+      <Route
+        path='/Creative'
+        render={props => <Home {...props} category={2} />}
+      />
+      <Route path='/code' component={CodePage} />
+      <Route path='/electronic' component={ElectronicMusic} />
+      <Route path='/' component={Home} />
     </Switch>
-  </Router>, document.getElementById('root'));
+  </Router>,
+  document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
